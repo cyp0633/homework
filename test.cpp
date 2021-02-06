4ec55d910ba9f02048ad6eb9f17aecd1c4cea019
cyp0633 <cyp0633@gmail.com>
2021-02-06 22:09:05 +0800

    整理repo & 测试GPG密钥

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+#include<iostream>
+#include<string>
+#include<format>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<format("PRINT {0}",a)<<'\n';
+    return 0;
+}
