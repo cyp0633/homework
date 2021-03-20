e9005fd5f43ebda11b10cc85727be35b4a4cf7b9
cyp0633 <cyp0633@gmail.com>
2021-03-20 13:23:26 +0800

    整理文件结构，加入数据结构作业

---
+++ ProgrammingDesign/1-Language Essentials/axis.s
@@ -0,0 +1,215 @@
+	.file	"axis.cpp"
+	.text
+	.section	.text$_Z5scanfPKcz,"x"
+	.linkonce discard
+	.globl	_Z5scanfPKcz
+	.def	_Z5scanfPKcz;	.scl	2;	.type	32;	.endef
+	.seh_proc	_Z5scanfPKcz
+_Z5scanfPKcz:
+.LFB2:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	pushq	%rbx
+	.seh_pushreg	%rbx
+	subq	$56, %rsp
+	.seh_stackalloc	56
+	leaq	128(%rsp), %rbp
+	.seh_setframe	%rbp, 128
+	.seh_endprologue
+	movq	%rcx, -48(%rbp)
+	movq	%rdx, -40(%rbp)
+	movq	%r8, -32(%rbp)
+	movq	%r9, -24(%rbp)
+	leaq	-40(%rbp), %rax
+	movq	%rax, -96(%rbp)
+	movq	-96(%rbp), %rbx
+	movl	$0, %ecx
+	movq	__imp___acrt_iob_func(%rip), %rax
+	call	*%rax
+	movq	%rbx, %r8
+	movq	-48(%rbp), %rdx
+	movq	%rax, %rcx
+	call	__mingw_vfscanf
+	movl	%eax, -84(%rbp)
+	movl	-84(%rbp), %eax
+	addq	$56, %rsp
+	popq	%rbx
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section	.text$_Z6printfPKcz,"x"
+	.linkonce discard
+	.globl	_Z6printfPKcz
+	.def	_Z6printfPKcz;	.scl	2;	.type	32;	.endef
+	.seh_proc	_Z6printfPKcz
+_Z6printfPKcz:
+.LFB8:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	pushq	%rbx
+	.seh_pushreg	%rbx
+	subq	$56, %rsp
+	.seh_stackalloc	56
+	leaq	128(%rsp), %rbp
+	.seh_setframe	%rbp, 128
+	.seh_endprologue
+	movq	%rcx, -48(%rbp)
+	movq	%rdx, -40(%rbp)
+	movq	%r8, -32(%rbp)
+	movq	%r9, -24(%rbp)
+	leaq	-40(%rbp), %rax
+	movq	%rax, -96(%rbp)
+	movq	-96(%rbp), %rbx
+	movl	$1, %ecx
+	movq	__imp___acrt_iob_func(%rip), %rax
+	call	*%rax
+	movq	%rbx, %r8
+	movq	-48(%rbp), %rdx
+	movq	%rax, %rcx
+	call	__mingw_vfprintf
+	movl	%eax, -84(%rbp)
+	movl	-84(%rbp), %eax
+	addq	$56, %rsp
+	popq	%rbx
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section .rdata,"dr"
+_ZStL19piecewise_construct:
+	.space 1
+.lcomm _ZStL8__ioinit,1,1
+	.globl	a
+	.bss
+	.align 16
+a:
+	.space 24
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "(%d,%d),(%d,%d),(%d,%d)\0"
+.LC1:
+	.ascii "[%d,%d]\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB1573:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$80, %rsp
+	.seh_stackalloc	80
+	.seh_endprologue
+	call	__main
+	leaq	20+a(%rip), %rax
+	movq	%rax, 48(%rsp)
+	leaq	16+a(%rip), %rax
+	movq	%rax, 40(%rsp)
+	leaq	12+a(%rip), %rax
+	movq	%rax, 32(%rsp)
+	leaq	8+a(%rip), %r9
+	leaq	4+a(%rip), %r8
+	leaq	a(%rip), %rdx
+	leaq	.LC0(%rip), %rcx
+	call	_Z5scanfPKcz
+	movl	$0, -4(%rbp)
+.L7:
+	cmpl	$2, -4(%rbp)
+	jg	.L6
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	leaq	4+a(%rip), %rax
+	movl	(%rdx,%rax), %ecx
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	leaq	a(%rip), %rax
+	movl	(%rdx,%rax), %eax
+	movl	%ecx, %r8d
+	movl	%eax, %edx
+	leaq	.LC1(%rip), %rcx
+	call	_Z6printfPKcz
+	addl	$1, -4(%rbp)
+	jmp	.L7
+.L6:
+	movl	$0, %eax
+	addq	$80, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__tcf_0;	.scl	3;	.type	32;	.endef
+	.seh_proc	__tcf_0
+__tcf_0:
+.LFB2055:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	call	_ZNSt8ios_base4InitD1Ev
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_Z41__static_initialization_and_destruction_0ii;	.scl	3;	.type	32;	.endef
+	.seh_proc	_Z41__static_initialization_and_destruction_0ii
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2054:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	cmpl	$1, 16(%rbp)
+	jne	.L12
+	cmpl	$65535, 24(%rbp)
+	jne	.L12
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	call	_ZNSt8ios_base4InitC1Ev
+	leaq	__tcf_0(%rip), %rcx
+	call	atexit
+.L12:
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_GLOBAL__sub_I_a;	.scl	3;	.type	32;	.endef
+	.seh_proc	_GLOBAL__sub_I_a
+_GLOBAL__sub_I_a:
+.LFB2056:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	$65535, %edx
+	movl	$1, %ecx
+	call	_Z41__static_initialization_and_destruction_0ii
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section	.ctors,"w"
+	.align 8
+	.quad	_GLOBAL__sub_I_a
+	.ident	"GCC: (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	__mingw_vfscanf;	.scl	2;	.type	32;	.endef
+	.def	__mingw_vfprintf;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt8ios_base4InitD1Ev;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
+	.def	atexit;	.scl	2;	.type	32;	.endef
