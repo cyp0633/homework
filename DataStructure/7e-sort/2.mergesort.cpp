b4deacdbf1986db97cae85f59a561352bf168033
cyp0633 <cyp0633@gmail.com>
2021-05-27 17:01:06 +0800

    Experiment 7 Shellsort

--- DataStructure/7e-sort/2.mergesort.cpp
+++ DataStructure/7e-sort/2.mergesort.cpp
@@ -4,36 +4,36 @@ using namespace std;
 
 void mergesort(int *A, int *temp, int left, int right)
 {
-    if(left==right)
+    if (left == right)
     {
         return;
     }
-    int mid=(left+right)/2;
-    mergesort(A,temp,left,mid);
-    mergesort(A,temp,mid+1,right);
-    for(int i=left;i<=right;i++)//memcpy?
+    int mid = (left + right) / 2;
+    mergesort(A, temp, left, mid);
+    mergesort(A, temp, mid + 1, right);
+    for (int i = left; i <= right; i++) //memcpy?
     {
-        temp[i]=A[i];
+        temp[i] = A[i];
     }
-    int i1=left;
-    int i2=mid+1;
-    for(int curr=left;curr<=right;curr++)
+    int i1 = left;
+    int i2 = mid + 1;
+    for (int curr = left; curr <= right; curr++)
     {
-        if(i1==mid+1)
+        if (i1 == mid + 1)
         {
-            A[curr]=temp[i2++];
+            A[curr] = temp[i2++];
         }
-        else if(i2>right)
+        else if (i2 > right)
         {
-            A[curr]=temp[i1++];
+            A[curr] = temp[i1++];
         }
-        else if(temp[i1]<temp[i2])
+        else if (temp[i1] < temp[i2])
         {
-            A[curr]=temp[i1++];
+            A[curr] = temp[i1++];
         }
         else
         {
-            A[curr]=temp[i2++];
+            A[curr] = temp[i2++];
         }
     }
 }
