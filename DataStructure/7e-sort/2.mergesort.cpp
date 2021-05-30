89d6ae22ad98fb5e7b8cb2e2cb3bae740bc23804
cyp0633 <cyp0633@gmail.com>
2021-05-30 19:20:36 +0800

    Add some notes for 7-2 Mergesort

--- DataStructure/7e-sort/2.mergesort.cpp
+++ DataStructure/7e-sort/2.mergesort.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 void mergesort(int *A, int *temp, int left, int right)
@@ -11,27 +11,27 @@ void mergesort(int *A, int *temp, int left, int right)
     int mid = (left + right) / 2;
     mergesort(A, temp, left, mid);
     mergesort(A, temp, mid + 1, right);
-    for (int i = left; i <= right; i++) //memcpy?
+    for (int i = left; i <= right; i++) //memcpy?先将数字加进temp区域
     {
         temp[i] = A[i];
     }
     int i1 = left;
     int i2 = mid + 1;
-    for (int curr = left; curr <= right; curr++)
+    for (int curr = left; curr <= right; curr++) //只是一个计数器，放回一个数字加一个
     {
-        if (i1 == mid + 1)
+        if (i1 == mid + 1) //左半边的数字消耗完毕，加右边的
         {
             A[curr] = temp[i2++];
         }
-        else if (i2 > right)
+        else if (i2 > right) //右半边的数字消耗完毕，加左边的
         {
             A[curr] = temp[i1++];
         }
-        else if (temp[i1] < temp[i2])
+        else if (temp[i1] < temp[i2]) //左边的小，加左边的
         {
             A[curr] = temp[i1++];
         }
-        else
+        else //右边的小，加右边的
         {
             A[curr] = temp[i2++];
         }
