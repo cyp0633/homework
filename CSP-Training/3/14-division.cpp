434c401bc660f1ced6f1f97f6c03547c4058cd8d
cyp0633 <cyp0633@gmail.com>
2021-09-09 09:37:40 +0800

    CSP 3-14 division A除以B Add notes

--- CSP-Training/3/14-division.cpp
+++ CSP-Training/3/14-division.cpp
@@ -8,23 +8,23 @@ int main()
     string aStr;
     int a[1001], b, len;
     cin >> aStr >> b;
-    len = aStr.length();
-    if (len <= 1 && stoi(aStr) < b)
+    len = aStr.length();            //a的长度
+    if (len <= 1 && stoi(aStr) < b) //a<b的情况特判，一定是商为0余数为a
     {
         printf("0 %d", stoi(aStr));
         return 0;
     }
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++) //将a从字符串中转移到int数组中存储，同时将ASCII码转化为数字
     {
-        a[i] = aStr[i] - '0';
+        a[i] = aStr[i] - '0'; //'1'-'0'==1
     }
     for (int i = 0; i < len; i++)
     {
-        a[i + 1] += a[i] % b * 10;
-        a[i] /= b;
-        if (i == 0 && a[i] == 0)
+        a[i + 1] += a[i] % b * 10; //继承上一位的余数
+        a[i] /= b;                 //进行这一位的除法运算
+        if (i == 0 && a[i] == 0)   //得出的前导0不输出，i==0因为这个0只能存在于第一位
             continue;
-        printf("%d", a[i]);
+        printf("%d", a[i]); //输出这一位的除法结果
     }
-    printf(" %d", a[len] / 10);
+    printf(" %d", a[len] / 10); //输出最后的余数，就是个位除之后的余数
 }
