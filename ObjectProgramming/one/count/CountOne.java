0e58507adfc3d381d7b7e7bcb4f80e83e2e20ee0
cyp0633 <cyp0633@gmail.com>
2021-10-13 18:42:18 +0800

    Object 1-7 Count 1 计算int型二进制1的个数

---
+++ ObjectProgramming/one/count/CountOne.java
@@ -0,0 +1,18 @@
+package ObjectProgramming.one.count;
+
+//计算int型二进制的个数
+import java.util.*;
+
+public class CountOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num, count = 0;
+        num = sc.nextInt();
+        while (num > 0) {
+            count += num % 2;
+            num /= 2;
+        }
+        System.out.printf("%d", count);
+        sc.close();
+    }
+}
