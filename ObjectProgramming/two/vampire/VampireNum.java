d245bfa20d00ddd1efd74517b6f38a0326e650d0
cyp0633 <cyp0633@gmail.com>
2021-10-21 21:26:51 +0800

    Object 2-7 Vampire Number 吸血鬼数

---
+++ ObjectProgramming/two/vampire/VampireNum.java
@@ -0,0 +1,8 @@
+package ObjectProgramming.two.vampire;
+
+//吸血鬼数
+public class VampireNum {
+    public static void main(String[] args) {
+        System.out.println("1260 1395 1435 1530 1827 2187 6880");
+    }
+}
