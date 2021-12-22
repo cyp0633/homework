c4a134aa0c2e38e1a24fab178b42a7a1a0f3cd25
cyp0633 <cyp0633@gmail.com>
2021-12-22 22:49:34 +0800

    Object - Generic+Factory+Container+Singleton

--- ObjectProgramming/singleton/Factory.java
+++ ObjectProgramming/singleton/Factory.java
@@ -1,14 +1,13 @@
 package ObjectProgramming.singleton;
 
 public class Factory<T> {
-    int id=0;
+    static int id = 0;
 
     static final Factory<A> a1 = new Factory<>();
     static final Factory<B> b1 = new Factory<>();
     static final Factory<C> c1 = new Factory<>();
 
     X createX() {
-        id+=1;
         if (this == a1) {
             System.out.printf("Creating an A\n");
             return new A(id);
@@ -18,29 +17,27 @@ public class Factory<T> {
         } else if (this == c1) {
             System.out.printf("Creating a C\n");
             return new C(id);
-        } else{
-            id-=1;
+        } else {
+            id -= 1;
             return null;
         }
     }
 
     public static X create(String s) {
-        if (s.equals("A"))
-            return a1.createX();
-        else if (s.equals("B"))
-            return b1.createX();
-        else if (s.equals("C"))
-            return c1.createX();
-        else
-            return null;
+        X temp = null;
+        id += 1;
+        if (s.equals("A")) {
+            temp = a1.createX();
+            ((A) temp).id = id;
+        } else if (s.equals("B")) {
+            temp = b1.createX();
+            ((B) temp).id = id;
+        } else if (s.equals("C")) {
+            temp = c1.createX();
+            ((C) temp).id = id;
+        }
+        if (temp == null)
+            id -= 1;
+        return temp;
     }
-
-    // public static void get(String[] args) {
-    //     Factory<?> f1 = Factory.create("A");
-    //     f1.createX();
-    //     Factory<?> f2 = Factory.create("B");
-    //     f2.createX();
-    //     Factory<?> f3 = Factory.create("C");
-    //     f3.createX();
-    // }
 }
--- ObjectProgramming/singleton/ObjectSpawner.java
+++ ObjectProgramming/singleton/ObjectSpawner.java
@@ -6,11 +6,24 @@ import java.util.*;
 public class ObjectSpawner {
 
     public static void main(String[] args) {
-        List<X> objList=new ArrayList<X>();
+        List<X> objList = new ArrayList<X>();
+        Class<? extends X> classType;
         objList.add(Factory.create("A"));
         objList.add(Factory.create("B"));
-        for(int i=0;i<objList.size();i++){
-            System.out.printf("Class: %s, ID: %d\n",objList.get(i).getClass(),objList.get(i).id);
+        objList.add(Factory.create("C"));
+        for (int i = 0; i < objList.size(); i++) {
+            classType = objList.get(i).getClass();
+            System.out.printf("Class: %s, ID:", classType.getName());
+            if (classType.getName().equals("ObjectProgramming.singleton.A")) {
+                A tempA = (A) objList.get(i);
+                System.out.printf("%d\n", tempA.id);
+            } else if (classType.getName().equals("ObjectProgramming.singleton.B")) {
+                B tempB = (B) objList.get(i);
+                System.out.printf("%d\n", tempB.id);
+            } else if (classType.getName().equals("ObjectProgramming.singleton.C")) {
+                C tempC = (C) objList.get(i);
+                System.out.printf("%d\n", tempC.id);
+            }
         }
     }
 }
